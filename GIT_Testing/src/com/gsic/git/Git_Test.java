ae2258fcabfc99821e3953c140dff3f667c7c83c
LinTo1 <LinTo1@ON34C02018313.cihs.ad.gov.on.ca>
2016-07-25 22:44:07 -0400

    initial checking
    Signed-off-by: LinTo1 <LinTo1@ON34C02018313.cihs.ad.gov.on.ca>

---
+++ GIT_Testing/src/com/gsic/git/Git_Test.java
@@ -0,0 +1,19 @@
+package com.gsic.git;
+
+public class Git_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SOAB_Print soab = new SOAB_Print();
+		soab.print();
+		printNames("Gary Li");
+		
+	}
+	
+	public static void printNames(String m_input) {
+		System.out.println("Here is the SOAB Team List");
+		System.out.println("--" + m_input);
+	}
+
+}
---
+++ GIT_Testing/src/com/gsic/git/SOAB_Print.java
@@ -0,0 +1,7 @@
+package com.gsic.git;
+
+public class SOAB_Print {
+	public void print() {
+		System.out.println("This is a SOAB Project");
+	}
+}
